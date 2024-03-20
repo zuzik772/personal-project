0b7b33fc852292ecc4034c9b14fc02550d966630
Zuzana Chudinova <zuzka.chudinova@gmail.com>
2024-03-20 20:14:53 +0100

    create user

--- src/app/api/users/route.ts
+++ src/app/api/users/route.ts
@@ -1,5 +1,41 @@
+import prisma from "@/lib/prisma";
 import { NextResponse } from "next/server";
+import { hash } from "bcrypt";
 
-export async function GET() {
-  return NextResponse.json({ message: "Hello from the API" });
+export async function POST(req: Request) {
+  try {
+    const body = await req.json();
+    const { email, username, password } = body;
+
+    const existingUser = await prisma.user.findUnique({ where: { email } });
+
+    if (existingUser) {
+      return NextResponse.json(
+        { user: null, message: "User already exists" },
+        { status: 409 }
+      );
+    }
+    const saltRounds = 10;
+    const hashedPassword = await hash(password, saltRounds);
+    const newUSer = await prisma.user.create({
+      data: {
+        email,
+        username,
+        password: hashedPassword,
+      },
+    });
+    const { password: newUserPassword, ...rest } = newUSer;
+    return NextResponse.json(
+      {
+        user: rest,
+        message: "User created successfully",
+      },
+      { status: 201 }
+    );
+  } catch (error) {
+    return NextResponse.json(
+      { message: "Something went wrong" },
+      { status: 500 }
+    );
+  }
 }
