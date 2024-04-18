42cecb0f86d779e797a4e516850f33b37fbd6ce0
Zuzana Chudinova <zuzka.chudinova@gmail.com>
2024-04-18 20:43:13 +0200

    add skeleton for task loading

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className={`${inter.className} antialiased`}>
         <NextTopLoader height={4} showSpinner={false} />
         <AuthProvider>
           <main className="h-screen flex justify-center items-center">
